157302b08fa1e20b830d0b21f8588f25c7efb566
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-11-11 07:44:41 -0600

    variant colors

--- colors/tokens.js
+++ colors/tokens.js
@@ -1,9 +1,11 @@
 import brand from "./src/brand";
 import global from "./src/global";
+import variant from "./src/variant";
 
 export default {
   colors: {
     brand,
     global,
+    variant,
   },
 };
