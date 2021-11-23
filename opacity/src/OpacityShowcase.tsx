1f4c2f4341408d03ea0768b70eccf0fbf6bf0d54
Kyle Knight <122591+undefined@users.noreply.github.com>
2021-11-23 20:47:03 +0000

    opacity

--- opacity/src/OpacityShowcase.tsx
+++ opacity/src/OpacityShowcase.tsx
@@ -1,56 +1,42 @@
 import * as React from "react";
 import styled from "styled-components";
 
-const Box = styled.div`
-  position: relative;
-  height: 100px;
-  width: 100px;
-  border: 1px solid #000;
-  border-radius: 4px;
-`;
-
 const CheckeredBox = styled.div`
-  opacity: 1;
   background-color: #000000;
   background-image: linear-gradient(45deg, #999 25%, transparent 25%),
     linear-gradient(45deg, transparent 75%, #999 75%),
-    linear-gradient(45deg, transparent 75%, #999 75%),
-    linear-gradient(45deg, #999 25%, #fff 25%);
-  background-size: 1rem 1rem;
-  background-position: 0 0, 0 0, -0.5rem -0.5rem, 0.5rem 0.5rem;
-  position: absolute;
-  top: 0;
-  z-index: -1;
+      linear-gradient(45deg, transparent 75%, #999 75%),
+        linear-gradient(45deg, #999 25%, #fff 25%);
+  background-size: 1.2rem 1.2rem;
+  background-position: 0 0, 0 0, -0.6rem -0.6rem, 0.6rem 0.6rem;
   box-shadow: none;
-  width: 100%;
-  height: 100%;
+  height: 100px;
+  width: 100px;
 `;
 
 const OpacityShowcase = ({ theme }) => {
   const { colors, opacity } = theme.default;
 
-  const out = Object.keys(opacity).map((alpha) => {
+  return Object.keys(opacity).map((alpha) => {
     return (
-      <>
-        <Box>
-          <CheckeredBox>
-            <div
-              key={alpha}
-              style={{
-                backgroundColor: colors.brand.primary,
-                opacity: opacity[alpha],
-                width: "100%",
-                height: "100%",
-              }}
-            />
-          </CheckeredBox>
-        </Box>
+      <React.Fragment key={alpha}>
+        <CheckeredBox>
+          <div
+            style={{
+              backgroundColor: colors.brand.primary,
+              width: "100px",
+              height: "100px",
+              position: "relative",
+              opacity: opacity[alpha],
+            }}
+          />
+        </CheckeredBox>
+
         <caption>theme.default.opacity.{alpha}</caption>
-      </>
+        <caption>--default-opacity-{alpha}</caption>
+      </React.Fragment>
     );
   });
-
-  return out;
 };
 
 export default OpacityShowcase;
