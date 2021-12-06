8d952ab3fd8bee2dd2ff9deec8fca0125593d66a
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-12-06 12:29:04 -0600

    SawmillPlayground

--- layout/src/SawmillPlayground.js
+++ layout/src/SawmillPlayground.js
@@ -1,8 +1,8 @@
 import { ShadowPlayground } from '@divriots/dockit-react/playground';
 import styles from 'bootstrap/dist/css/bootstrap.css?raw';
 
-const SawmillPlayground = ({ ...props }) => {
-  return <ShadowPlayground styles={styles} {...props} />;
-};
+const SawmillPlayground = ({ ...props }) => (
+  <ShadowPlayground styles={styles} {...props} />
+);
 
 export default SawmillPlayground;
