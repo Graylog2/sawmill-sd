bbf5b7a91f5c7c88d9d3f68438e698b99267de4e
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-12-06 12:18:30 -0600

    SawmillPlayground

---
+++ layout/src/SawmillPlayground.js
@@ -0,0 +1,6 @@
+import { ShadowPlayground } from '@divriots/dockit-react/playground';
+import styles from 'bootstrap/dist/css/bootstrap.css?raw';
+
+export const SawmillPlayground = (props) => {
+  return <ShadowPlayground styles={styles} {...props} />;
+};
--- layout/src/index.tsx
+++ layout/src/index.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import {
-  CssLayout,
-  getInitialColorScheme,
-} from '@divriots/dockit-react/mdx-layout-css';
+import { CssLayout } from '@divriots/dockit-react/mdx-layout-css';
 import { Logo } from './Logo';
 import * as theme from '~/theme/src/theme.json';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
+import { SawmillPlayground } from './SawmillPlayground';
 import '~/theme/src/variables.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'https://unpkg.com/prismjs@1.25.0/themes/prism-tomorrow.css';
 
-const noirVars = async () => await import('~/theme/src/variables-noir.css');
-
 const GlobalStyle = createGlobalStyle`
   @import '../../theme/src/variables.css';
-  @import 'bootstrap/dist/css/bootstrap.css';
 
   @media (prefers-color-scheme: dark) {
     :root {
@@ -37,33 +32,17 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const DarkColors = createGlobalStyle`
-  @media (prefers-color-scheme: dark) {
-    :root {
-      ${noirVars()}
-    }
-  }
-`;
-
 export const Layout = (props: unknown) => {
-  const [mode, setMode] = React.useState(getInitialColorScheme());
-
-  const handleModeSwitch = (newMode) => {
-    setMode(newMode);
-  };
-
   return (
     <ThemeProvider theme={theme}>
-      <MDXProvider components={{}}>
+      <MDXProvider components={{ SawmillPlayground }}>
         <GlobalStyle />
-        {mode === 'dark' ? <DarkColors /> : null}
         <CssLayout
           logo={
             <div style={{ width: '250px' }}>
               <Logo />
             </div>
           }
-          onSwitch={handleModeSwitch}
           {...props}
         />
       </MDXProvider>
