3559039d66c5696e9ede5178298fe02b2ac3fa1e
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-12-06 16:44:26 -0600

    SawmillPlayground

--- layout/src/index.tsx
+++ layout/src/index.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
-import { CssLayout } from '@divriots/dockit-react/mdx-layout-css';
+import { MDXProvider } from '@mdx-js/react';
+import {
+  CssLayout,
+  getInitialColorScheme,
+} from '@divriots/dockit-react/mdx-layout-css';
 import { Logo } from './Logo';
 import * as theme from '~/theme/src/theme.json';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
-import SawmillPlayground from './SawmillPlayground';
+import '~/theme/src/variables.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'https://unpkg.com/prismjs@1.25.0/themes/prism-tomorrow.css';
+
+const noirVars = async () => await import('~/theme/src/variables-noir.css');
 
 const GlobalStyle = createGlobalStyle`
   @import '../../theme/src/variables.css';
-  @import 'bootstrap/dist/css/bootstrap.css';
 
   @media (prefers-color-scheme: dark) {
     :root {
@@ -30,19 +36,36 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const DarkColors = createGlobalStyle`
+  @media (prefers-color-scheme: dark) {
+    :root {
+      ${noirVars()}
+    }
+  }
+`;
+
 export const Layout = (props: unknown) => {
+  const [mode, setMode] = React.useState(getInitialColorScheme());
+
+  const handleModeSwitch = (newMode) => {
+    setMode(newMode);
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <CssLayout
-        logo={
-          <div style={{ width: '250px' }}>
-            <Logo />
-          </div>
-        }
-        components={{ SawmillPlayground }}
-        {...props}
-      />
+      <MDXProvider components={{}}>
+        <GlobalStyle />
+        {mode === 'dark' ? <DarkColors /> : null}
+        <CssLayout
+          logo={
+            <div style={{ width: '250px' }}>
+              <Logo />
+            </div>
+          }
+          onSwitch={handleModeSwitch}
+          {...props}
+        />
+      </MDXProvider>
     </ThemeProvider>
   );
 };
