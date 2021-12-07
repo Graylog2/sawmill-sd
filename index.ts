20cd416b4a760d7b628e1c24ce4198ccfedd6129
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-12-07 15:21:59 -0600

    different export

--- index.ts
+++ index.ts
@@ -5,14 +5,24 @@ import themeJson from './theme/src/theme.json';
 import themeCss from './theme/src/variables.css';
 import themeNoirCss from './theme/src/variables-noir.css';
 
-export default {
-  components: {
-    Alert,
-    Button,
-  },
-  theme: {
-    json: themeJson,
-    css: themeCss,
+export const components = {
+  Alert,
+  Button,
+};
+
+export const theme = {
+  json: themeJson,
+  css: {
+    base: themeCss,
     noir: themeNoirCss,
   },
+  styles: `:root {
+    ${themeCss}
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      ${themeNoirCss}
+    }
+  }`,
 };
