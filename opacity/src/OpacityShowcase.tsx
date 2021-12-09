901b6e2295251cf45669b965600b4473168c8dce
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-12-09 15:22:39 -0600

    opacity docs

--- opacity/src/OpacityShowcase.tsx
+++ opacity/src/OpacityShowcase.tsx
@@ -1,43 +1,34 @@
 import * as React from 'react';
-import styled from 'styled-components';
+import { getCssCustomProps } from '@divriots/dockit-react/style-showcases';
+import { Showcases } from '@divriots/dockit-react/showcases';
+import './styles.css';
+import { createGlobalStyle } from 'styled-components';
 
-const CheckeredBox = styled.div`
-  background-color: #000000;
-  background-image: linear-gradient(45deg, #999 25%, transparent 25%),
-    linear-gradient(45deg, transparent 75%, #999 75%),
-    linear-gradient(45deg, transparent 75%, #999 75%),
-    linear-gradient(45deg, #999 25%, #fff 25%);
-  background-size: 1.2rem 1.2rem;
-  background-position: 0 0, 0 0, -0.6rem -0.6rem, 0.6rem 0.6rem;
-  box-shadow: none;
-  height: 100px;
-  width: 100px;
+const props = getCssCustomProps('--sawmill-opacities');
+const values = props.map(([name]) => name);
+
+const opacityClassNames = values
+  .map((value) => {
+    return `.${value} { opacity: var(${value}); }`;
+  })
+  .join('\n');
+
+const GlobalStyle = createGlobalStyle`
+  ${opacityClassNames}
 `;
 
-const OpacityShowcase = ({ theme }) => {
-  const { colors, opacities } = theme;
-
-  return Object.keys(opacities).map((opacity) => {
-    return (
-      <React.Fragment key={opacity}>
-        <CheckeredBox>
-          <div
-            style={{
-              backgroundColor: colors.brand.primary,
-              width: '100px',
-              height: '100px',
-              position: 'relative',
-              opacity: opacities[opacity],
-            }}
-          />
-        </CheckeredBox>
-
-        <caption style={{ whiteSpace: 'nowrap' }}>
-          --sawmill-opacity-{opacity}
-        </caption>
-      </React.Fragment>
-    );
-  });
+export const OpacityShowcase = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Showcases
+        showcaseClasses={values}
+        showcaseComponent="box"
+        componentProps={{
+          className: 'box',
+          checkeredBackground: true,
+        }}
+      />
+    </>
+  );
 };
-
-export default OpacityShowcase;
