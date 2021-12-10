f684da61815e1e3aec10cf3677936e302c9e1daf
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-12-10 07:03:18 -0600

    typography docs

--- sd.config.mjs
+++ sd.config.mjs
@@ -1,67 +1,73 @@
-import StyleDictionary from 'style-dictionary';
+import StyleDictionary from "style-dictionary";
 
-const buildPath = 'theme/src/';
+const buildPath = "theme/src/";
 
 StyleDictionary.registerFormat({
-  name: 'css/variables',
-  formatter: function ({ dictionary, options }) {
-    return `${options.mode === 'root' ? `:root` : `.${options.mode}`} {
+  name: "css/variables",
+  formatter: function({ dictionary, options }) {
+    const selector = options.selector ? `.${options.selector}` : `:root`;
+
+    return `${selector} {
       ${dictionary.allProperties
       .map((prop) => {
-        let value = options.mode === 'root' ? prop.value : prop[`${options.mode}Value`];
+        let value = options.selector ? prop[`${options.selector}Value`] : prop.value;
 
         if (options.outputReferences) {
           if (dictionary.usesReference(prop.original.value)) {
-            const reference = dictionary.getReferences(prop.original.value);
+            const references = dictionary.getReferences(prop.original.value);
 
-            value = reference[0].name;
-            return `  --${prop.name}: var(--${value});`;
+            references.forEach(ref => {
+              if (ref.value && ref.name) {
+                value = value.replace(ref.value, function() {
+                  return `var(--${ref.name})`
+                });
+              }
+            });
           }
         }
-        
+
         return `  --${prop.name}: ${value};`;
       })
-      .join('\n')}
+      .join("\n")}
     }`;
-  },
+  }
 });
 
 StyleDictionary.extend({
-  source: ['{colors,elevations,opacity,radii,spacings,typography}/**/*.json'],
+  source: ["{colors,elevations,opacity,radii,spacings,typography}/**/*.json"],
   platforms: {
     css: {
-      transformGroup: 'css',
-      prefix: 'sawmill',
+      transformGroup: "css",
+      prefix: "sawmill",
       buildPath,
       files: [
         {
-          destination: 'variables.css',
-          format: 'css/variables',
+          destination: "variables.css",
+          format: "css/variables",
           options: {
-            mode: 'root',
             outputReferences: true
           }
         },
         {
-          destination: 'variables-noir.css',
-          format: 'css/variables',
+          destination: "variables-noir.css",
+          format: "css/variables",
           filter: ({ noirValue }) => noirValue,
           options: {
-            mode: 'noir',
+            selector: "noir",
             outputReferences: true
           }
-        },
-      ],
+        }
+      ]
     },
     js: {
-      transformGroup: 'web',
+      transformGroup: "web",
       buildPath,
       files: [
         {
-          destination: 'theme.json',
-          format: 'json/nested',
-        },
-      ],
-    },
-  },
+          destination: "theme.json",
+          format: "json/nested"
+        }
+      ]
+    }
+  }
 }).buildAllPlatforms();
