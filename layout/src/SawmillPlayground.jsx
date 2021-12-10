5d2540f01af29b83f542c97a9c1432e871fa73f3
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2021-12-10 10:43:54 -0600

    playground styles

--- layout/src/SawmillPlayground.jsx
+++ layout/src/SawmillPlayground.jsx
@@ -1,25 +1,17 @@
 import * as React from 'react';
-import { ShadowPlayground } from '@divriots/dockit-react/playground';
+import { Playground } from '@divriots/dockit-react/playground';
 import bootstrapStyles from 'bootstrap/dist/css/bootstrap.css?raw';
 import sawmillVars from '~/theme/src/variables.css?raw';
 import noirSawmillVars from '~/theme/src/variables-noir.css?raw';
 
 const styles = `
-  :root {
-    ${sawmillVars}
-  }
-  
-  @media (prefers-color-scheme: dark) {
-    :root {
-      ${noirSawmillVars}
-    }
-  }
-  
+  ${sawmillVars}
+  ${noirSawmillVars}
   ${bootstrapStyles}
 `;
 
 const SawmillPlayground = ({ ...props }) => (
-  <ShadowPlayground styles={styles} {...props} />
+  <Playground styles={styles} {...props} />
 );
 
 export default SawmillPlayground;
